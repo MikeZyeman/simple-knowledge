b7d5a274a60f8676d744625117059d0a12411b9e
MikeZyeman <mi.zyeman@gmail.com>
2020-12-28 22:32:22 +0100

    Removed Delete and GetAll function

--- libs/fql-lib/src/lib/collections.ts
+++ libs/fql-lib/src/lib/collections.ts
@@ -1,20 +1,12 @@
 import * as faunadb from 'faunadb';
 
 import * as dotenv from 'dotenv';
-import { query } from 'faunadb';
-import Collection = query.Collection;
 dotenv.config();
 
 const client = new faunadb.Client({ secret: process.env.FAUNADB_KEY})
 const {
-  Delete,
-  CreateCollection,
-  Collections
+  CreateCollection
 } = faunadb.query;
-
-export const getAllCollection = async () => {
-  return await client.query(Collections)
-}
 
 export const createCollection = async (tableName: string) => {
   return await client.query(
@@ -22,8 +14,3 @@ export const createCollection = async (tableName: string) => {
   )
 }
 
-export const deleteCollection = async (tableName: string) => {
-  return await client.query(
-    Delete(Collection(tableName))
-  )
-}
